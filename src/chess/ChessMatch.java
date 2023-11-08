c0f2a39b9ab764924308b9d6005f5764bff2df4a
Joao Victor Lopes <joaovlpssdev@gmail.com>
2023-11-08 18:32:13 -0300

    Implemented ChessPosition (A1 - H8) Implemented placeNewPiece, which uses ChessPosition instead of position 	modified:   src/chess/ChessMatch.java 	new file:   src/chess/ChessPosition.java 	new file:   src/chess/exceptions/ChessException.java

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -24,8 +24,14 @@ public class ChessMatch {
         return matrix;
     }
 
+    /* Receives ChessPosition column and row values, and a ChessPiece.
+     * Passes the values to placePiece, converted to Position values, for compatibility. */
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
     }
 }
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,45 @@
+package chess;
+
+import boardgame.Position;
+import chess.exceptions.ChessException;
+import boardgame.Position;
+
+public class ChessPosition {
+    private char column;
+    private int row;
+
+    public ChessPosition(char column, int row){
+        if (column < 'a' || column > 'h' || row < 0 || row > 8){
+            throw new ChessException("Position out of bounds. Valid values are a1 - h8.");
+        }
+        this.column = column;
+        this.row = row;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    /* Converts ChessPositions to board matrix positions. 
+     * Matrix rows are equal to 8 - ChessPosition rows
+     * Matrix columns are equal to ChessPosition columns - 'a'
+     * (because of Unicode encoding).
+    */
+    protected Position toPosition(){
+        return new Position(8 - row, column - 'a');
+    }
+
+    // The inverse of toPosition
+    protected static ChessPosition fromPosition(Position position){
+        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+    }
+
+    @Override
+    public String toString(){
+        return "" + column + row; // String concat to return column and row as Strings.
+    }
+}
