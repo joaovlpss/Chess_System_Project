cd529c26d9db57a5873ff6e7d79b3323d1e9b60e
Joao Victor Lopes <joaovlpssdev@gmail.com>
2023-11-10 17:03:46 -0300

    Added move count and pawn first move logic 	modified:   src/chess/ChessMatch.java 	modified:   src/chess/ChessPiece.java 	modified:   src/chess/pieces/Pawn.java

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -23,10 +23,19 @@ public class Pawn extends ChessPiece {
         if (getColor() == Color.WHITE){ // White pawns can only move upwards
             // Setting p to the row above the pawn 
             // Checking if p has any pieces or not
+            // If it's the first move, the Pawn can move two squares forward.
+
             p.setValues(position.getRow() - 1, position.getColumn());
             if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
                 matrix[p.getRow()][p.getColumn()] = true;
             }
+            // If it's the first move, the Pawn can move two squares forward.
+            p.setValues(position.getRow() - 2, position.getColumn());
+            Position p2 = new Position(position.getRow() - 1, position.getColumn()); // But only if the first square above it isn't occupied.
+            if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0){
+                matrix[p.getRow()][p.getColumn()] = true;
+            }
+
 
             // Checking if there are any opponent pieces to the right of the pawn
             p.setValues(position.getRow() - 1, position.getColumn() + 1);
@@ -46,6 +55,12 @@ public class Pawn extends ChessPiece {
             // Checking if p has any pieces or not
             p.setValues(position.getRow() + 1, position.getColumn());
             if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+                matrix[p.getRow()][p.getColumn()] = true;
+            }
+            // If it's the first move, the Pawn can move two squares forward.
+            p.setValues(position.getRow() + 2, position.getColumn());
+            Position p2 = new Position(position.getRow() + 1, position.getColumn()); // But only if the first square below it isn't occupied.
+            if(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0){
                 matrix[p.getRow()][p.getColumn()] = true;
             }
 
